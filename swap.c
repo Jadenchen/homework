c56fae7e6f276d435ae5b9c991f8395408167728
jadenchen <menzxisland@gmail.com>
2016-03-11 17:49:21 +0800

    modify swap

--- swap.c
+++ swap.c
@@ -8,11 +8,29 @@ void swap (int *a, int *b){
 }
 
 
+void strreverse(char *s){
+
+ 	char *p = s;
+	while (p && *p)
+		++p;
+	for (--p; s < p; ++s, --p){
+		*s = *s^*p;
+		*p = *s^*p;
+		*s = *p^*s;
+	}
+}
+
 int main (void){
 	int a = 4;
 	int b = 2;
 	printf ("a = %d, b = %d\n", a,b);
 	swap(&a, &b);
 	printf ("a = %d, b = %d\n", a,b);
+	
+	char s[] = "ABCDEF";
+	printf ("s is %s\n",s);
+	strreverse(s);
+	printf("reverse s is %s\n", s);
+
 	return 0;
 }
